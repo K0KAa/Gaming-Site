0597fb396f863a535f407a2e4634fc1f1bddc30f
K0KAa <kushalkattel0@gmail.com>
2021-11-26 17:30:54 +0545

    Comming soon msg

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SocialMedia() {
     return (
-        <div>
-            
+        <div style={{display:'flex'}}>
+            COMMING SOON
         </div>
     )
 }
--- src/Components/Sponsers.js
+++ src/Components/Sponsers.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Sponsers() {
     return (
-        <div>
-            
+        <div style={{display:'flex'}}>
+            COMMING SOON
         </div>
     )
 }
